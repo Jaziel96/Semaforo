52c9a1a19ffd6ebf3fa59072a0974e8778d83985
Jaziel96 <jazcov96@hotmail.com>
2022-07-28 02:40:08 +0000

    Luces semaforo

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [color,setColor]=useState("")
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="semaforo">
+		<div>
+			<div className={`rojo ${"rojo"==color?"active":""}`} onClick={()=>{setColor("rojo")}}></div>
+			<div className={`amarillo ${"amarillo"==color?"active":""}`} onClick={()=>{setColor("amarillo")}}></div>
+			<div className={`verde ${"verde"==color?"active":""}`} onClick={()=>{setColor("verde")}}></div>
 		</div>
+	</div>
 	);
 };
 
